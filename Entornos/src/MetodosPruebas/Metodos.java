17909896fa68f061a55d33766cf2d0c458faef7f
vaain <vaain@DESKTOP-E7E5TPS>
2023-01-20 16:34:15 +0100

    Cambio tipos de datos y bucle Ejemplo......................

---
+++ Entornos/src/MetodosPruebas/Metodos.java
@@ -0,0 +1,67 @@
+package MetodosPruebas;
+
+public class Metodos {
+
+	public boolean esPar(int a) {
+		boolean par = true;
+		if (a % 2 == 0) {
+			par = true;
+		} else {
+			par = false;
+		}
+		return par;
+	}
+	
+	public int esMayor(int a, int b) {
+		int mayor = 0;
+		if (a > b) {
+			mayor = a;
+		} else {
+			mayor = b;
+		}
+		return mayor;
+	}
+	
+	public boolean esPrimo(int a) {
+		boolean primo = true;
+		if (a == 0 || a == 1) {
+			primo = false;
+		} else {
+			for (int i = 2; i <= (a / 2); i++) {
+				if (a % i == 0) {
+					primo = false;
+				}
+			}
+		}
+		return primo;
+	}
+	
+	public int suma(int a, int b) {
+		int resultado = a + b;
+		return resultado;
+
+	}
+	
+	public int resta(int a, int b) {
+		int resultado = a - b;
+		return resultado;
+
+	}
+	
+	public int multi(int a, int b) {
+		int resultado = a * b;
+		return resultado;
+
+	}
+	
+	public double divi(double a, double b) {
+		double resultado = a / b;
+		if (b == 0) {
+			return -1;
+		} else {
+			resultado = a / b;
+			return resultado;
+		}
+	}
+
+}
